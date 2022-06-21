bbe57ad603f5073480b7ed57d6f521928913850a
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-21 18:51:22 -0500

    Add django api rest framework for app views

--- Encora_Workplace/Encora_Workplace/urls.py
+++ Encora_Workplace/Encora_Workplace/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from encora_app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.ListUsers, basename='User')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', views.ListUsers.as_view()),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
