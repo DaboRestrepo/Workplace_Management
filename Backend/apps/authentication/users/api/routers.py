4f5e2b825b74bcf708fdd0436d11aa1b4edf951b
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-29 17:48:51 -0500

    Add Authentication with token

--- Backend/apps/authentication/users/api/routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .viewsets import UserViewset
-
-router = DefaultRouter()
-
-router.register(r'user', UserViewset, basename='user')
-
-urlpatterns = router.urls
--- Backend/apps/authentication/users/api/viewsets.py
+++
@@ -1,8 +0,0 @@
-from ..models import UsersModel
-from rest_framework.viewsets import ModelViewSet
-from .serializers import UserSerializer
-
-
-class UserViewset(ModelViewSet):
-    queryset = UsersModel.objects.all()
-    serializer_class = UserSerializer
