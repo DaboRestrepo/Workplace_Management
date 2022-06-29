331dd5cc484caea89571b424faf1487f206b2a21
DaboRestrepo <daborestrepo@gmail.com>
2022-06-28 20:26:57 -0500

    Erased the login from the user

--- Backend/encora_workplace/urls.py
+++ Backend/encora_workplace/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from apps.authentication.users.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
